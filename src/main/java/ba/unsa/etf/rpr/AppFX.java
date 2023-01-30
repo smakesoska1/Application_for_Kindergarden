63ea4564c1daa9425c796a74bbc3c84ccf37d006
smakesoska1 <smakesoska1@etf.unsa.ba>
2023-01-30 17:35:18 +0100

    login form made

--- src/main/java/ba/unsa/etf/rpr/AppFX.java
+++ src/main/java/ba/unsa/etf/rpr/AppFX.java
@@ -11,7 +11,7 @@ import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 public class AppFX extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/loginForm.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         primaryStage.setResizable(false);
