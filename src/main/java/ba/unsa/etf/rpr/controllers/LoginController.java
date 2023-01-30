571de37f5f5e5e25aa3368de8fca00178037abe5
smakesoska1 <smakesoska1@etf.unsa.ba>
2023-01-30 19:26:50 +0100

    id fields and onAction setup

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/LoginController.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    public TextField usernameid;
+    public Button cancelButtonId;
+
+
+}
