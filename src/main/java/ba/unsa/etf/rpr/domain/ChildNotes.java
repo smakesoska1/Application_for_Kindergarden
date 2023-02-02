641a76b263967df6d6da0cc2521fc780bfc387ec
smakesoska1 <smakesoska1@etf.unsa.ba>
2023-02-02 11:07:41 +0100

    getters and setters for child notes

--- src/main/java/ba/unsa/etf/rpr/domain/ChildNotes.java
+++ src/main/java/ba/unsa/etf/rpr/domain/ChildNotes.java
@@ -1,4 +1,22 @@
 package ba.unsa.etf.rpr.domain;
 
 public class ChildNotes {
+    private int id;
+    private String noteName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNoteName() {
+        return noteName;
+    }
+
+    public void setNoteName(String noteName) {
+        this.noteName = noteName;
+    }
 }
