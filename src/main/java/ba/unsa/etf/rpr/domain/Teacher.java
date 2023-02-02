666b382772031a0b3c9dbad39e83608fef8d8f02
smakesoska1 <smakesoska1@etf.unsa.ba>
2023-02-02 11:48:22 +0100

    teacher class exstending person

--- src/main/java/ba/unsa/etf/rpr/domain/Teacher.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Teacher.java
@@ -1,4 +1,42 @@
 package ba.unsa.etf.rpr.domain;
 
-public class Teacher {
+import java.time.LocalTime;
+
+public class Teacher extends Person{
+    private String username;
+    private String password;
+    private LocalTime startWork;
+    private LocalTime endWork;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalTime getStartWork() {
+        return startWork;
+    }
+
+    public void setStartWork(LocalTime startWork) {
+        this.startWork = startWork;
+    }
+
+    public LocalTime getEndWork() {
+        return endWork;
+    }
+
+    public void setEndWork(LocalTime endWork) {
+        this.endWork = endWork;
+    }
 }
