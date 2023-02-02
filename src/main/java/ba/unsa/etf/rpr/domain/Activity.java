38effc883ad171e01e129a4921e9cdc3161f8d8d
smakesoska1 <smakesoska1@etf.unsa.ba>
2023-02-02 11:15:13 +0100

    hash code for activity

--- src/main/java/ba/unsa/etf/rpr/domain/Activity.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Activity.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr.domain;
 
+import java.util.Objects;
+
 public class Activity {
     private int id;
     private String activityName;
@@ -18,5 +20,20 @@ public class Activity {
 
     public void setActivityName(String activityName) {
         this.activityName = activityName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Activity activity = (Activity) o;
+
+        return id == activity.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id,activityName);
     }
 }
