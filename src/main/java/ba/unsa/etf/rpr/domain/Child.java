340b27518acee4e15dce857b8b0836b95043510a
smakesoska1 <smakesoska1@etf.unsa.ba>
2023-02-02 11:56:19 +0100

    child class getters and setters

--- src/main/java/ba/unsa/etf/rpr/domain/Child.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Child.java
@@ -1,4 +1,61 @@
 package ba.unsa.etf.rpr.domain;
 
-public class Child {
+import java.time.LocalTime;
+
+public class Child extends Person{
+
+    private Parent parent;
+    private Teacher teacher;
+    private Activity activity;
+    private ChildNotes childNotes;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    public Parent getParent() {
+        return parent;
+    }
+
+    public void setParent(Parent parent) {
+        this.parent = parent;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public Activity getActivity() {
+        return activity;
+    }
+
+    public void setActivity(Activity activity) {
+        this.activity = activity;
+    }
+
+    public ChildNotes getChildNotes() {
+        return childNotes;
+    }
+
+    public void setChildNotes(ChildNotes childNotes) {
+        this.childNotes = childNotes;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
 }
