94fbd52fe8f794527e23b8bf90083e13a20c05f5
smakesoska1 <smakesoska1@etf.unsa.ba>
2023-02-02 12:31:21 +0100

    interface dao and classes

---
+++ src/main/java/ba/unsa/etf/rpr/dao/Dao.java
@@ -0,0 +1,15 @@
+package ba.unsa.etf.rpr.dao;
+
+import java.util.List;
+
+public interface Dao<T>{
+    T getById(int id);
+
+    T add(T item);
+
+    T update(T item);
+
+    void delete(int id);
+
+    List<T> getAll();
+}
