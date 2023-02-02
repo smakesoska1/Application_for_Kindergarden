af763e8fe9f2bbe42b08128ab26dd150e8a53f04
smakesoska1 <smakesoska1@etf.unsa.ba>
2023-02-02 11:10:40 +0100

    hash code for child notes

--- src/main/java/ba/unsa/etf/rpr/domain/ChildNotes.java
+++ src/main/java/ba/unsa/etf/rpr/domain/ChildNotes.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr.domain;
 
+import java.util.Objects;
+
 public class ChildNotes {
     private int id;
     private String noteName;
@@ -26,5 +28,20 @@ public class ChildNotes {
                 "id=" + id +
                 ", noteName='" + noteName + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ChildNotes that = (ChildNotes) o;
+
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id,noteName);
     }
 }
