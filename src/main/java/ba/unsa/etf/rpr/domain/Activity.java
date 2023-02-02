e2aa0b2112f26dc7cab06d4aebbb1289352bc095
smakesoska1 <smakesoska1@etf.unsa.ba>
2023-02-02 11:14:32 +0100

    getters and setters for activity

--- src/main/java/ba/unsa/etf/rpr/domain/Activity.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Activity.java
@@ -1,4 +1,22 @@
 package ba.unsa.etf.rpr.domain;
 
 public class Activity {
+    private int id;
+    private String activityName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getActivityName() {
+        return activityName;
+    }
+
+    public void setActivityName(String activityName) {
+        this.activityName = activityName;
+    }
 }
