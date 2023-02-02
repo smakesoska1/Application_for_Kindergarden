781a6693310a62e7d799cb495b2e334611c16241
smakesoska1 <smakesoska1@etf.unsa.ba>
2023-02-02 11:41:23 +0100

    director class exstending person

--- src/main/java/ba/unsa/etf/rpr/domain/Director.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Director.java
@@ -1,4 +1,31 @@
 package ba.unsa.etf.rpr.domain;
 
-public class Director {
+public class Director extends Person{
+    private String username;
+    private String password;
+    private int phoneNumber;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
--- src/main/java/ba/unsa/etf/rpr/domain/Person.java
+++ src/main/java/ba/unsa/etf/rpr/domain/Person.java
@@ -1,4 +1,67 @@
 package ba.unsa.etf.rpr.domain;
 
+import java.util.Objects;
+
 public class Person {
+    private int id;
+    private String firstName;
+    private String surname;
+    private String adress;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        return id == person.id;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
+
+    public int hashCode() {
+        return Objects.hash(id,firstName,surname,adress);
+    }
+
+
+
 }
