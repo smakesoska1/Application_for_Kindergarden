a4fd25359a16ad2c055b5bfa37cedd674c161f90
smakesoska1 <smakesoska1@etf.unsa.ba>
2023-02-05 13:25:44 +0100

    addes child note

--- src/main/java/ba/unsa/etf/rpr/dao/ChildNotesDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ChildNotesDaoSQLImpl.java
@@ -42,8 +42,38 @@ public class ChildNotesDaoSQLImpl implements ChildNotesDao{
         return null;
     }
 
+    private int getMaxId(){
+        int id_note=0;
+        try {
+            PreparedStatement statement = this.conn.prepareStatement("SELECT MAX(id_note) FROM child_notes");
+            ResultSet rs = statement.executeQuery();
+            if(rs.next()) {
+                id_note = rs.getInt(1);
+                rs.close();
+                return id_note;
+            }
+        } catch (SQLException e) {
+            System.out.println("Problem pri radu sa bazom podataka");
+            System.out.println(e.getMessage());
+        }
+        return id_note;
+    }
+
+
     @Override
     public ChildNotes add(ChildNotes item) {
+        int id_note=getMaxId()+1;
+        try {
+            PreparedStatement stmt = this.conn.prepareStatement("INSERT INTO child_notes (id_note,note_name) VALUES (?,?)");
+            stmt.setInt(1,id_note);
+            stmt.setString(2, item.getNoteName());
+            stmt.executeUpdate();
+            item.setId(id_note);
+            return item;
+        } catch (SQLException e) {
+            System.out.println("Problem pri radu sa bazom podataka");
+            System.out.println(e.getMessage());
+        }
         return null;
     }
 
