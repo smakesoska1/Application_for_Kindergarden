78a643007b6b0eff0c27ac1d2342031f127b8783
smakesoska1 <smakesoska1@etf.unsa.ba>
2023-02-05 14:24:37 +0100

    getBYId for parents

--- src/main/java/ba/unsa/etf/rpr/dao/ParentDaoSQLImpl.java
+++ src/main/java/ba/unsa/etf/rpr/dao/ParentDaoSQLImpl.java
@@ -1,10 +1,9 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.domain.ChildNotes;
 import ba.unsa.etf.rpr.domain.Parent;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
 
 public class ParentDaoSQLImpl implements ParentDao,PersonDao{
@@ -22,8 +21,32 @@ public class ParentDaoSQLImpl implements ParentDao,PersonDao{
 
     @Override
     public Parent getById(int id) {
+
+        try {
+            PreparedStatement stmt = this.conn.prepareStatement("SELECT * FROM parent WHERE id_parent = ?");
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()){
+                Parent parent = new Parent();
+                parent.setId(rs.getInt("id_parent"));
+                parent.setFirstName(rs.getString("parent_name"));
+                parent.setSurname(rs.getString("parent_surname"));
+                parent.setAdress(rs.getString("parent_adress"));
+                parent.setUsername(rs.getString("parent_username"));
+                parent.setPassword(rs.getString("parent_password"));
+                parent.setPhoneNumber(rs.getInt("parent_phone"));
+                rs.close();
+                return parent;
+            }else{
+                return null;
+            }
+        } catch (SQLException e) {
+            System.out.println("Problem pri radu sa bazom podataka");
+            System.out.println(e.getMessage());
+        }
         return null;
     }
+
 
     @Override
     public Parent add(Parent item) {
