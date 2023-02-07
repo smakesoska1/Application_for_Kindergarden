c154ebba98e8b34ae9e91d03d525e870a2c22399
smakesoska1 <smakesoska1@etf.unsa.ba>
2023-02-07 11:54:08 +0100

    dao factory

--- src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
+++ src/main/java/ba/unsa/etf/rpr/dao/DaoFactory.java
@@ -1,4 +1,39 @@
 package ba.unsa.etf.rpr.dao;
 
+import ba.unsa.etf.rpr.domain.Parent;
+
 public class DaoFactory {
+    private static final DirectorDao directorDao = new DirectorDaoSQLImpl();
+    private static final TeacherDao teacherDao = new TeacherDaoSQLImpl();
+    private static final ParentDao parentDao = new ParentDaoSQLImpl();
+    private static final ChildDao childDao = new ChildDaoSQLImpl();
+    private static final ActivityDao activityDao = new ActivityDaoSQLImpl();
+    private static final ChildNotesDao childNotesDao = new ChildNotesDaoSQLImpl();
+
+    private DaoFactory(){
+    }
+
+    public static DirectorDao directorDao(){
+        return directorDao;
+    }
+
+    public static TeacherDao teacherDao(){
+        return teacherDao;
+    }
+
+    public static ParentDao parentDao(){
+        return parentDao;
+    }
+
+    public static ChildDao childDao(){
+        return childDao;
+    }
+
+    public static ActivityDao activityDao(){
+        return activityDao;
+    }
+
+    public static ChildNotesDao childNotesDao(){
+        return childNotesDao;
+    }
 }
