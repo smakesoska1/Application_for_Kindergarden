c934a1490c717753d5e5c9d81e76021457a34ea6
smakesoska1 <smakesoska1@etf.unsa.ba>
2023-02-08 12:10:03 +0100

    parent manager username done

--- src/main/java/ba/unsa/etf/rpr/business/ParentManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/ParentManager.java
@@ -1,4 +1,39 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.ChildNotes;
+import ba.unsa.etf.rpr.domain.Parent;
+import ba.unsa.etf.rpr.exceptions.KindergardenException;
+
+import java.util.List;
+
 public class ParentManager {
+
+    public void validateUsername(String username) throws KindergardenException {
+        if (username == null || username.length() > 45 || username.length() < 2){
+            throw new KindergardenException("Username must be between 2 and 45 chars");
+        }
+    }
+
+    public List<Parent> getAll() throws KindergardenException {
+        return DaoFactory.parentDao().getAll();
+    }
+
+    public void delete(int id) throws KindergardenException{
+        DaoFactory.parentDao().delete(id);
+    }
+
+    public Parent getById(int parentId) throws KindergardenException{
+        return DaoFactory.parentDao().getById(parentId);
+    }
+
+    public void update(Parent p) throws KindergardenException{
+        validateUsername(p.getUsername());
+        DaoFactory.parentDao().update(p);
+    }
+
+    public Parent add(Parent p) throws KindergardenException{
+        validateUsername(p.getUsername());
+        return DaoFactory.parentDao().add(p);
+    }
 }
