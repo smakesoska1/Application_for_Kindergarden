15d111cd71a91bae28fe19c5e8d25f58f56bd23b
smakesoska1 <smakesoska1@etf.unsa.ba>
2023-02-08 12:37:00 +0100

    child manager main implementation

--- src/main/java/ba/unsa/etf/rpr/business/ChildManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/ChildManager.java
@@ -1,6 +1,39 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Child;
+import ba.unsa.etf.rpr.exceptions.KindergardenException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChildManager {
 
+    public List<Child> getAll() throws KindergardenException {
+        return DaoFactory.childDao().getAll();
+    }
 
+    public void delete(int id) throws KindergardenException{
+        DaoFactory.childDao().delete(id);
+    }
+
+    public Child getById(int childId) throws KindergardenException{
+        return DaoFactory.childDao().getById(childId);
+    }
+
+    public void update(Child cn) throws KindergardenException{
+        DaoFactory.childDao().update(cn);
+    }
+
+    public Child add(Child cn) throws KindergardenException{
+        return DaoFactory.childDao().add(cn);
+    }
+
+    public ArrayList<Child> searchChildrenOfParent(int parentId) throws KindergardenException{
+        return DaoFactory.childDao().searchChildrenOfParent(parentId);
+    }
+
+    public ArrayList<Child> searchChildrenOfTeacher(int teacherId) throws KindergardenException{
+        return DaoFactory.childDao().searchChildrenOfTeacher(teacherId);
+    }
 }
