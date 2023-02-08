83858b602952176245025875a2399f34c3664fa2
smakesoska1 <smakesoska1@etf.unsa.ba>
2023-02-08 14:45:51 +0100

    two tests for activity

--- src/test/java/ba/unsa/etf/rpr/ActivityManagerTest.java
+++ src/test/java/ba/unsa/etf/rpr/ActivityManagerTest.java
@@ -4,10 +4,18 @@ import ba.unsa.etf.rpr.dao.ActivityDaoSQLImpl;
 import ba.unsa.etf.rpr.domain.Activity;
 import ba.unsa.etf.rpr.exceptions.KindergardenException;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.sql.SQLClientInfoException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
-public class ActivityManager {
+public class ActivityManagerTest {
+
     @Test
     public void testUpdateActivity() throws KindergardenException {
         ActivityDaoSQLImpl activityDao=new ActivityDaoSQLImpl();
@@ -28,4 +36,22 @@ public class ActivityManager {
         //assertNotEquals(orginal,newName);
         assertEquals(newName, updateAc);
     }
+    @Test
+    public void testGetAll() throws KindergardenException{
+        ActivityDaoSQLImpl activityDao = Mockito.mock(ActivityDaoSQLImpl.class);
+        List<Activity> expectedActivities = new ArrayList<>();
+        Activity activity1 = new Activity();
+        activity1.setId(1);
+        activity1.setActivityName("Activity 1");
+        expectedActivities.add(activity1);
+        Activity activity2 = new Activity();
+        activity2.setId(2);
+        activity2.setActivityName("Activity 2");
+        expectedActivities.add(activity2);
+        when(activityDao.getAll()).thenReturn(expectedActivities);
+
+        List<Activity> retrievedActivities = activityDao.getAll();
+        assertEquals(expectedActivities, retrievedActivities);
+    }
+
 }
