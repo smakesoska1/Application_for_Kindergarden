f3b8278e64eebb713b8a7c27d0cf30593bc57627
smakesoska1 <smakesoska1@etf.unsa.ba>
2023-02-08 10:31:52 +0100

    activity manager implementation

--- src/main/java/ba/unsa/etf/rpr/business/ActivityManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/ActivityManager.java
@@ -1,4 +1,30 @@
 package ba.unsa.etf.rpr.business;
 
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.Activity;
+import ba.unsa.etf.rpr.exceptions.KindergardenException;
+
+import java.util.List;
+
 public class ActivityManager {
+    public List<Activity> getAll() throws KindergardenException{
+        return DaoFactory.activityDao().getAll();
+    }
+
+    public void delete(int id) throws KindergardenException{
+        DaoFactory.activityDao().delete(id);
+    }
+
+    public Activity getById(int activityId) throws KindergardenException{
+        return DaoFactory.activityDao().getById(activityId);
+    }
+
+    public void update(Activity a) throws KindergardenException{
+        DaoFactory.activityDao().update(a);
+    }
+
+    public Activity add(Activity a) throws KindergardenException{
+        return DaoFactory.activityDao().add(a);
+    }
+
 }
