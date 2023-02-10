5476e9deba4990e2db99520c7d250d312b6f2c62
smakesoska1 <smakesoska1@etf.unsa.ba>
2023-02-10 12:32:44 +0100

    teacher test

---
+++ src/test/java/ba/unsa/etf/rpr/TeacherTest.java
@@ -0,0 +1,18 @@
+package ba.unsa.etf.rpr;
+
+import ba.unsa.etf.rpr.business.DirectorManager;
+import ba.unsa.etf.rpr.business.TeacherManager;
+import ba.unsa.etf.rpr.domain.Teacher;
+import ba.unsa.etf.rpr.exceptions.KindergardenException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TeacherTest {
+
+    @Test
+    public void searchByUsernameTeacherTest() throws KindergardenException {
+        TeacherManager manager=new TeacherManager();
+        Teacher username=manager.searchTeacherByUsername("teacher1");
+        Assertions.assertEquals("Amina",username.getFirstName());
+    }
+}
