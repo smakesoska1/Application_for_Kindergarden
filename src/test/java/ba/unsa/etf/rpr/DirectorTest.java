924f80e65169e9446f0f71aca7228386f2b76f77
smakesoska1 <smakesoska1@etf.unsa.ba>
2023-02-10 12:22:20 +0100

    director test

---
+++ src/test/java/ba/unsa/etf/rpr/DirectorTest.java
@@ -0,0 +1,18 @@
+package ba.unsa.etf.rpr;
+
+import ba.unsa.etf.rpr.business.DirectorManager;
+import ba.unsa.etf.rpr.exceptions.KindergardenException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DirectorTest {
+
+    @Test
+    public void searchByUsernameDirectorTest() throws KindergardenException {
+
+        DirectorManager manager=new DirectorManager();
+        Assertions.assertEquals(manager.searchDirectorByUsername("nhalilovic1"),null);
+    }
+
+
+}
