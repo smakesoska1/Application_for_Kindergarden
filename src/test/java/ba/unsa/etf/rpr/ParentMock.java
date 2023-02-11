604412ffefdfa3b0412695f9d5af4d3cb7fe219b
smakesoska1 <smakesoska1@etf.unsa.ba>
2023-02-11 13:55:23 +0100

    another mock test for adding a parent

--- src/test/java/ba/unsa/etf/rpr/ParentMock.java
+++ src/test/java/ba/unsa/etf/rpr/ParentMock.java
@@ -3,22 +3,49 @@ package ba.unsa.etf.rpr;
 import ba.unsa.etf.rpr.business.ParentManager;
 import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.dao.ParentDao;
+import ba.unsa.etf.rpr.dao.ParentDaoSQLImpl;
 import ba.unsa.etf.rpr.dao.PersonDao;
 import ba.unsa.etf.rpr.domain.Parent;
 import ba.unsa.etf.rpr.domain.Person;
 import ba.unsa.etf.rpr.exceptions.KindergardenException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ParentMock {
+    private ParentDaoSQLImpl parentDao;
+    private ParentManager parentManager;
+    List<Parent> parents;
+
+    @BeforeEach
+    public void initialize(){
+        parentDao = Mockito.mock(ParentDaoSQLImpl.class);
+        parentManager = Mockito.mock(ParentManager.class);
+        parents=new ArrayList<>();
+
+        Parent parent = new Parent();
+        parent.setId(5);
+        parent.setFirstName("Adnan");
+        parent.setSurname("Hodzic");
+        parent.setAdress("Maglajska");
+        parent.setUsername("ahodzic2");
+        parent.setPassword("ahodzic2");
+        parent.setPhoneNumber(378645896);
+
+        parents.add(parent);
+    }
+
 
     @Test
     public void validateIfParentExist() throws KindergardenException {
             MockedStatic<DaoFactory> dao= Mockito.mockStatic(DaoFactory.class);
-            ParentDao parentDao=Mockito.mock(ParentDao.class);
+            parentDao=Mockito.mock(ParentDaoSQLImpl.class);
             Mockito.when(DaoFactory.parentDao()).thenReturn(parentDao);
 
         Parent parent = new Parent();
@@ -31,5 +58,21 @@ public class ParentMock {
 
         assertTrue(value);
         dao.close();
+    }
+
+    @Test
+    public void addParent() throws KindergardenException {
+        Parent parent = new Parent();
+        parent.setId(5);
+        parent.setFirstName("Lejla");
+        parent.setSurname("Begic");
+        parent.setAdress("Hiseta");
+        parent.setUsername("lbegic1");
+        parent.setPassword("lbegic1");
+        parent.setPhoneNumber(378645896);
+
+        parentManager.add(parent);
+        Mockito.verify(parentManager).add(parent);
+
     }
 }
