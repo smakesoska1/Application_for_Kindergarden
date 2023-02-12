be2ff0ad3200353b02f1cc19bd14e92316be135c
smakesoska1 <smakesoska1@etf.unsa.ba>
2023-02-12 20:13:21 +0100

    refactor of directorScreen.fxml and DirectorScreenController

--- src/main/java/ba/unsa/etf/rpr/controllers/DirectorScreenController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/DirectorScreenController.java
@@ -1,4 +1,15 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
 public class DirectorScreenController {
+    @FXML
+    private Button logOutBtn;
+
+    public void logOut(){
+        Stage stage = (Stage) logOutBtn.getScene().getWindow();
+        stage.close();
+    }
 }
