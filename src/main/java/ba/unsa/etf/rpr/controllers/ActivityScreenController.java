3950467224d9d04e9af572ac588419a9c6a4daad
smakesoska1 <smakesoska1@etf.unsa.ba>
2023-02-13 14:41:04 +0100

    added close button for add activity fxml

--- src/main/java/ba/unsa/etf/rpr/controllers/ActivityScreenController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ActivityScreenController.java
@@ -1,4 +1,25 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class ActivityScreenController {
+    @FXML
+    private TextField textFieldActivity;
+    @FXML
+    private Button btnClose;
+
+
+    public void addSave(){
+
+    }
+
+    public void close(ActionEvent event){
+        Stage stage = (Stage) btnClose.getScene().getWindow();
+        stage.close();
+
+    }
 }
