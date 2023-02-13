4d53dc18a850b81617f421d8a94200469c257c57
smakesoska1 <smakesoska1@etf.unsa.ba>
2023-02-13 18:35:33 +0100

    adding notes into the list

--- src/main/java/ba/unsa/etf/rpr/controllers/NoteScreenController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/NoteScreenController.java
@@ -1,4 +1,36 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.business.ActivityManager;
+import ba.unsa.etf.rpr.business.ChildNotesManager;
+import ba.unsa.etf.rpr.domain.Activity;
+import ba.unsa.etf.rpr.domain.Child;
+import ba.unsa.etf.rpr.domain.ChildNotes;
+import ba.unsa.etf.rpr.exceptions.KindergardenException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
 public class NoteScreenController {
+    @FXML
+    private TextField textFieldNote;
+    @FXML
+    private Button closeBtn;
+
+    public void addNote() throws KindergardenException {
+        ChildNotes notes =new ChildNotes();
+        ChildNotesManager manager=new ChildNotesManager();
+        if(!textFieldNote.getText().isEmpty()){
+            notes.setNoteName(textFieldNote.getText());
+            manager.add(notes);
+            Stage stage = (Stage) textFieldNote.getScene().getWindow();
+            stage.close();
+        }
+    }
+
+    public void close(ActionEvent event){
+        Stage stage = (Stage) closeBtn.getScene().getWindow();
+        stage.close();
+    }
 }
