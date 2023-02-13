fa66f4ba29745bb06125ccfca8dfb45e5430817c
smakesoska1 <smakesoska1@etf.unsa.ba>
2023-02-13 10:01:48 +0100

    added getters for selected item in director home controller

--- src/main/java/ba/unsa/etf/rpr/controllers/DirectorScreenController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/DirectorScreenController.java
@@ -4,6 +4,7 @@ import ba.unsa.etf.rpr.business.ActivityManager;
 import ba.unsa.etf.rpr.business.ChildManager;
 import ba.unsa.etf.rpr.business.ParentManager;
 import ba.unsa.etf.rpr.business.TeacherManager;
+import ba.unsa.etf.rpr.domain.*;
 import ba.unsa.etf.rpr.exceptions.KindergardenException;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
@@ -21,11 +22,42 @@ public class DirectorScreenController {
      private ListView activitiList;
     @FXML
      private ListView parentList;
+    @FXML
+      private Button deleteParentBtn;
+    @FXML
+       private Button deleteActivityBtn;
+
+    private Child selectedChild;
+    private Teacher selectedTeacher;
+    private Activity selectedActivity;
+    private Parent selectedParent;
+    private ChildNotes selectedNotes;
+
 
     ChildManager managerc=new ChildManager();
     TeacherManager managert=new TeacherManager();
     ActivityManager managera=new ActivityManager();
     ParentManager managerp=new ParentManager();
+
+    public Child getSelectedChild() {
+        return selectedChild;
+    }
+
+    public Teacher getSelectedTeacher() {
+        return selectedTeacher;
+    }
+
+    public Activity getSelectedActivity() {
+        return selectedActivity;
+    }
+
+    public Parent getSelectedParent() {
+        return selectedParent;
+    }
+
+    public ChildNotes getSelectedNotes() {
+        return selectedNotes;
+    }
 
     public void logOut(){
         Stage stage = (Stage) logOutBtn.getScene().getWindow();
